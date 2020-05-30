347ccd0d4e9720c321a8b861766cded758e6b912
Jessica <31293603+JessicaLucindaCheng@users.noreply.github.com>
2020-05-30 11:16:36 -0700

    Updated DatabaseConnector.java with private variables

--- src/main/java/com/tomerestservice/db/DatabaseConnector.java
+++ src/main/java/com/tomerestservice/db/DatabaseConnector.java
@@ -4,7 +4,12 @@ import java.sql.*;
 
 
 public class DatabaseConnector {
-
+	private final static String DRIVER = "org.mariadb.jdbc.Driver";
+	private final static String URL = "jdbc:mariadb://127.0.0.1/tomedatabase";
+//	private final static String URL = "jdbc:mysql://127.0.0.1:3306"; // previous url used in pa3
+	private final static String USERNAME = "root";
+	private final static String PASSWORD = "";
+	
 
     private DatabaseConnector() {
 
@@ -13,17 +18,17 @@ public class DatabaseConnector {
     public static Connection getConnection() {
 
         try {
-            Class.forName("org.mariadb.jdbc.Driver");
+            Class.forName(DRIVER);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        
         try {
-            return DriverManager.getConnection("jdbc:mariadb://127.0.0.1/tomedatabase",
-                    "root", "");
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-
+        
         return null;
     }
 
