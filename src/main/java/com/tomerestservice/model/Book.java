80c92cdb4a6df5cc655d8d83168f81f9582e75f3
Hugo Ruiz <hruizvil@uci.edu>
2020-05-30 21:24:59 -0700

    added the books api methods

--- src/main/java/com/tomerestservice/model/Book.java
+++ src/main/java/com/tomerestservice/model/Book.java
@@ -6,32 +6,107 @@ import javax.xml.bind.annotation.XmlRootElement;
 //You will need to create a Java Object. Jersey uses these to construct requests and responses.
 
 public class Book {
-    private String summary;
-    private String description;
-    private int id;
-
-    public String getSummary() {
-        return summary;
-    }
-
-    public void setSummary(String summary) {
-        this.summary = summary;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
+  private String id;
+  private String author;
+  private String genre;
+  private String name;
+  private int price;
+  private String type;
+  private String image;
+  private String summary;
+  
+  public String getId() {
+	  return id;
+  }
+  
+  public void setId(String id) {
+	  this.id = id;
+  }
+  
+  public String getAuthor() {
+	  return author;
+  }
+  
+  public void setAuthor(String author) {
+	  this.author = author;
+  }
+  
+  public String getGenre() {
+	  return genre;
+  }
+  
+  public void setGenre(String genre) {
+	  this.genre = genre;
+  }
+  
+  public String getName() {
+	  return name;
+  }
+  
+  public void setName(String name) {
+	  this.name = name;
+  }
+  
+  public int getPrice() {
+	  return price;
+  }
+  
+  public void setPrice(int price) {
+	  this.price = price;
+  }
+  
+  public String getType() {
+	  return type;
+  }
+  
+  public void setType(String type) {
+	  this.type = type;
+  }
+  
+  public String getImage() {
+	  return image;
+  }
+  
+  public void setImage(String image) {
+	  this.image = image;
+  }
+  
+  public String getSummary() {
+	  return summary;
+  }
+  
+  public void setSummary(String summary) {
+	  this.summary = summary;
+  }
 }
+
+//public class Book {
+//    private String summary;
+//    private String description;
+//    private int id;
+//
+//    public String getSummary() {
+//        return summary;
+//    }
+//
+//    public void setSummary(String summary) {
+//        this.summary = summary;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//}
--- src/main/java/com/tomerestservice/service/BookService.java
+++ src/main/java/com/tomerestservice/service/BookService.java
@@ -13,22 +13,26 @@ import com.tomerestservice.model.Book;
 public class BookService {
 
 
-    private final static String ALL_TODOS_QUERY = "SELECT * FROM TODOS";
+    private final static String ALL_TODOS_QUERY = "SELECT * FROM books";
 
-    public static Book getBookById(int id) {
+    public static Book getBookById(String id) {
         //Get a new connection object before going forward with the JDBC invocation.
         Connection connection = DatabaseConnector.getConnection();
-        ResultSet resultSet = DatabaseUtils.retrieveQueryResults(connection, ALL_TODOS_QUERY + " WHERE TODO_ID = " + id);
+        ResultSet resultSet = DatabaseUtils.retrieveQueryResults(connection, ALL_TODOS_QUERY + " WHERE id="+ "\""+id+"\"");
 
         if (resultSet != null) {
             try {
                 while (resultSet.next()) {
                     Book book = new Book();
 
-
-                    book.setId(resultSet.getInt("TODO_ID"));
-                    book.setDescription(resultSet.getString("TODO_DESC"));
-                    book.setSummary(resultSet.getString("TODO_SUMMARY"));
+                    book.setId(resultSet.getString("id"));
+                    book.setAuthor(resultSet.getString("author"));                   	
+                    book.setGenre(resultSet.getString("genre"));
+                    book.setName(resultSet.getString("name"));
+                    book.setPrice(resultSet.getInt("price"));
+                    book.setType(resultSet.getString("type"));
+                    book.setImage(resultSet.getString("image"));
+                    book.setSummary(resultSet.getString("summary"));
 
                     return book;
 
@@ -47,8 +51,6 @@ public class BookService {
         }
 
         return null;
-
-
     }
 
     public static List<Book> getAllBooks() {
@@ -60,13 +62,18 @@ public class BookService {
         if (resultSet != null) {
             try {
                 while (resultSet.next()) {
-                    Book todo = new Book();
+                	Book book = new Book();
 
-                    todo.setId(resultSet.getInt("TODO_ID"));
-                    todo.setDescription(resultSet.getString("TODO_DESC"));
-                    todo.setSummary(resultSet.getString("TODO_SUMMARY"));
+                    book.setId(resultSet.getString("id"));
+                    book.setAuthor(resultSet.getString("author"));                   	
+                    book.setGenre(resultSet.getString("genre"));
+                    book.setName(resultSet.getString("name"));
+                    book.setPrice(resultSet.getInt("price"));
+                    book.setType(resultSet.getString("type"));
+                    book.setImage(resultSet.getString("image"));
+                    book.setSummary(resultSet.getString("summary"));
 
-                    books.add(todo);
+                    books.add(book);
 
                 }
             } catch (SQLException e) {
@@ -83,48 +90,48 @@ public class BookService {
         return books;
     }
 
-    public static boolean AddBook(Book book) {
+//    public static boolean AddBook(Book book) {
+//
+//        String sql = "INSERT INTO TODOS  (TODO_SUMMARY, TODO_DESC)" +
+//                "VALUES (?, ?)";
+//        Connection connection = DatabaseConnector.getConnection();
+//        return DatabaseUtils.performDBUpdate(connection, sql, book.getSummary(), book.getDescription());
+//
+//    }
 
-        String sql = "INSERT INTO TODOS  (TODO_SUMMARY, TODO_DESC)" +
-                "VALUES (?, ?)";
-        Connection connection = DatabaseConnector.getConnection();
-        return DatabaseUtils.performDBUpdate(connection, sql, book.getSummary(), book.getDescription());
+//    public static boolean updateBook(Book book) {
+//
+//        String sql = "UPDATE TODOS SET TODO_SUMMARY=?, TODO_DESC=? WHERE TODO_ID=?;";
+//
+//        Connection connection = DatabaseConnector.getConnection();
+//
+//        boolean updateStatus = DatabaseUtils.performDBUpdate(connection, sql, book.getSummary(), book.getDescription(),
+//                String.valueOf(book.getId()));
+//
+//        try {
+//            connection.close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return updateStatus;
+//
+//    }
 
-    }
-
-    public static boolean updateBook(Book book) {
-
-        String sql = "UPDATE TODOS SET TODO_SUMMARY=?, TODO_DESC=? WHERE TODO_ID=?;";
-
-        Connection connection = DatabaseConnector.getConnection();
-
-        boolean updateStatus = DatabaseUtils.performDBUpdate(connection, sql, book.getSummary(), book.getDescription(),
-                String.valueOf(book.getId()));
-
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return updateStatus;
-
-    }
-
-    public static boolean deleteBook(Book retrievedBook) {
-
-        String sql = "DELETE FROM TODOS WHERE TODO_ID=?;";
-
-        Connection connection = DatabaseConnector.getConnection();
-
-        boolean updateStatus = DatabaseUtils.performDBUpdate(connection, sql, String.valueOf(retrievedBook.getId()));
-
-        try {
-            connection.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return updateStatus;
-    }
+//    public static boolean deleteBook(Book retrievedBook) {
+//
+//        String sql = "DELETE FROM TODOS WHERE TODO_ID=?;";
+//
+//        Connection connection = DatabaseConnector.getConnection();
+//
+//        boolean updateStatus = DatabaseUtils.performDBUpdate(connection, sql, String.valueOf(retrievedBook.getId()));
+//
+//        try {
+//            connection.close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return updateStatus;
+//    }
 }
