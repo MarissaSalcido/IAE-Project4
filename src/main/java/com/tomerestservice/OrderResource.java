4711155320d487cf4aa6d37878c66ed761da8748
Jessica <31293603+JessicaLucindaCheng@users.noreply.github.com>
2020-05-31 17:59:58 -0700

    Update OrderResource.java

--- src/main/java/com/tomerestservice/OrderResource.java
+++ src/main/java/com/tomerestservice/OrderResource.java
@@ -14,49 +14,16 @@ import java.util.List;
  *
  */
 //todos will be the pathsegment that precedes any path segment specified by @Path on a method.
-@Path("/examples")
+@Path("/orders")
 public class OrderResource {
-
-
-    //This method represents an endpoint with the URL /todos/{id} and a GET request ( Note that {id} is a placeholder for a path parameter)
-    @Path("{id}")
-    @GET
-    @Produces( { MediaType.APPLICATION_JSON }) //This provides only JSON responses
-    public Response getExampleById(@PathParam("id") int id/* The {id} placeholder parameter is resolved */) {
-        //invokes the DB method which will fetch a todo_list item object by id
-        Example example = ExampleService.getExampleById(id);
-
-        //Respond with a 404 if there is no such todo_list item for the id provided
-        if(example == null) {
-            return Response.status(Response.Status.NOT_FOUND).build();
-        }
-
-        //Respond with a 200 OK if you have a todo_list_item object to return as response
-        return Response.ok(example).build();
-    }
-
-    //This method represents an endpoint with the URL /todos and a GET request.
-    // Since there is no @PathParam mentioned, the /todos as a relative path and a GET request will invoke this method.
-    @GET
-    @Produces( { MediaType.APPLICATION_JSON })
-    public Response getAllExamples() {
-        List<Example> exampleList = ExampleService.getAllExamples();
-
-        if(exampleList == null || exampleList.isEmpty()) {
-
-        }
-
-        return Response.ok(exampleList).build();
-    }
-
     //This method represents an endpoint with the URL /todos and a POST request.
     // Since there is no @PathParam mentioned, the /todos as a relative path and a POST request will invoke this method.
     @POST
     @Consumes({MediaType.APPLICATION_JSON}) //This method accepts a request of the JSON type
-    public Response addExample(Example example) {
+    public Response addOrder(Order order) {
 
         //The todo object here is automatically constructed from the json request. Jersey is so cool!
-        if(ExampleService.AddExample(example)) {
+        if(OrderService.AddOrder(order)) {
             return Response.ok().entity("TODO Added Successfully").build();
         }
 
@@ -65,18 +32,18 @@ public class OrderResource {
 
 
     }
-
+	
     //Similar to the method above.
     @POST
     @Consumes({MediaType.APPLICATION_FORM_URLENCODED}) //This method accepts form parameters.
     //If you were to send a POST through a form submit, this method would be called.
-    public Response addExample(@FormParam("summary") String summary,
+    public Response addOrder(@FormParam("summary") String summary,
                             @FormParam("description") String description) {
-        Example example = new Example();
-        example.setSummary(summary);
-        example.setDescription(description);
+        Order order = new Order();
+        order.setSummary(summary);
+        order.setDescription(description);
 
-        if(ExampleService.AddExample(example)) {
+        if(OrderService.AddOrder(order)) {
         	System.out.println("TODO added successfully");
         	
             return Response.ok().entity("TODO Added Successfully").build();
@@ -87,40 +54,72 @@ public class OrderResource {
 
     }
 
+
+    //This method represents an endpoint with the URL /todos/{id} and a GET request ( Note that {id} is a placeholder for a path parameter)
+/*    @Path("{id}")
+    @GET
+    @Produces( { MediaType.APPLICATION_JSON }) //This provides only JSON responses */
+//    public Response getOrderById(@PathParam("id") int id/* The {id} placeholder parameter is resolved */) {
+        //invokes the DB method which will fetch a todo_list item object by id
+ /*       Order order = OrderService.getOrderById(id);
+
+        //Respond with a 404 if there is no such todo_list item for the id provided
+        if(order == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        //Respond with a 200 OK if you have a todo_list_item object to return as response
+        return Response.ok(order).build();
+    }
+
+    //This method represents an endpoint with the URL /todos and a GET request.
+    // Since there is no @PathParam mentioned, the /todos as a relative path and a GET request will invoke this method.
+    @GET
+    @Produces( { MediaType.APPLICATION_JSON })
+    public Response getAllOrders() {
+        List<Order> orderList = OrderService.getAllOrders();
+
+        if(orderList == null || orderList.isEmpty()) {
+
+        }
+
+        return Response.ok(orderList).build();
+    }
+
     //This method represents a PUT request where the id is provided as a path parameter and the request body is provided in JSON
     @PUT
     @Path("{id}")
     @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
-    public Response updateExample(@PathParam("id") int id, Example example) {
+    public Response updateOrder(@PathParam("id") int id, Order order) {
 
         // Retrieve the todo that you will need to change
-        Example retrievedExample = ExampleService.getExampleById(id);
+        Order retrievedOrder = OrderService.getOrderById(id);
 
-        if(retrievedExample == null) {
+        if(retrievedOrder == null) {
             //If not found then respond with a 404 response.
             return Response.status(Response.Status.NOT_FOUND).
                     entity("We could not find the requested resource").build();
         }
 
         // This is the todo_object retrieved from the json request sent.
-        example.setId(id);
+        order.setId(id);
 
         // if the user has provided null, then we set the retrieved values.
         // This is done so that a null value is not passed to the todo object when updating it.
-        if(example.getDescription() == null) {
-            example.setDescription(retrievedExample.getDescription());
+        if(order.getDescription() == null) {
+            order.setDescription(retrievedOrder.getDescription());
         }
 
         //Same as above. We only change fields in the todo_resource when the user has entered something in a request.
-        if (example.getSummary() == null) {
-            example.setSummary(retrievedExample.getSummary());
+        if (order.getSummary() == null) {
+            order.setSummary(retrievedOrder.getSummary());
         }
 
         //This calls the JDBC method which in turn calls the the UPDATE SQL command
-        if(ExampleService.updateExample(example)) {
+        if(OrderService.updateOrder(order)) {
 
-            return Response.ok().entity(example).build();
+            return Response.ok().entity(order).build();
         }
 
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
@@ -132,26 +131,26 @@ public class OrderResource {
     @DELETE
     @Path("{id}")
     @Consumes({MediaType.APPLICATION_FORM_URLENCODED, MediaType.APPLICATION_JSON})
-    public Response deleteExample(@PathParam("id") int id) {
+    public Response deleteOrder(@PathParam("id") int id) {
 
         //Retrieve the todo_object that you want to delete.
-        Example retrievedExample = ExampleService.getExampleById(id);
+        Order retrievedOrder = OrderService.getOrderById(id);
 
-        if(retrievedExample == null) {
+        if(retrievedOrder == null) {
             //If not found throw a 404
             return Response.status(Response.Status.NOT_FOUND).
                     entity("We could not find the requested resource").build();
         }
 
         // This calls the JDBC method which in turn calls the DELETE SQL command.
-        if(ExampleService.deleteExample(retrievedExample)) {
+        if(OrderService.deleteOrder(retrievedOrder)) {
             return Response.ok().entity("TODO Deleted Successfully").build();
         }
 
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
 
 
-    }
+    }*/
 
 }
 
